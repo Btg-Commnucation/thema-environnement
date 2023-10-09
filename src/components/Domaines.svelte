2419a07afb4158e4bd6d8b4daf2e4de2501cc7e2
Tuvedunyel <mightygodx@gmail.com>
2023-10-09 10:08:43 +0200

    domaines

--- src/components/Domaines.svelte
+++ src/components/Domaines.svelte
@@ -5,6 +5,7 @@
   import he from "he";
   import { webpSupported, checkWebpSupport } from "stores/webpSupported";
   import { imageFormatter } from "stores/imageFormatter";
+  import DomainesContent from "@/lib/DomainesContent.svelte";
 
   checkWebpSupport();
 
@@ -46,6 +47,36 @@
       {@html data.content}
     </div>
   </div>
+</section>
+<section class="gradient-green">
+  <DomainesContent
+    titre={acf.titre_fond_vert}
+    columns={acf.column_fond_vert}
+    texte={acf.texte_fond_vert}
+    imageTitre="signature-thema-environnement-blanc.svg"
+    bulletColor="#fcbe00"
+    borderColor="white"
+  />
+</section>
+<section class="yellow">
+  <DomainesContent
+    titre={acf.titre_fond_jaune}
+    columns={acf.column_fond_jaune}
+    texte={acf.texte_fond_jaune}
+    imageTitre="signature-thema-environnement-gray.svg"
+    bulletColor="white"
+    borderColor="white"
+  />
+</section>
+<section class="white">
+  <DomainesContent
+    titre={acf.titre_fond_blanc}
+    columns={acf.column_fond_blanc}
+    texte={acf.texte_fond_blanc}
+    imageTitre="signature-thema-environnement-vert-clair.svg"
+    bulletColor="#44B389"
+    borderColor="#44B389"
+  />
 </section>
 
 <style lang="scss">
@@ -111,6 +142,30 @@
       margin: auto;
       width: 56.82px;
       height: 47.61px;
+    }
+  }
+
+  section.gradient-green {
+    background: var(--color-linear-gradient);
+    color: white;
+    padding-block: 5.5rem 3rem;
+
+    :global(a) {
+      color: white;
+    }
+  }
+
+  section.yellow {
+    background: var(--color-jaune);
+    padding-block: 5.5rem 3rem;
+  }
+
+  section.white {
+    padding-block: 5.5rem 3rem;
+    color: var(--color-vert);
+
+    :global(a) {
+      color: var(--color-vert);
     }
   }
 </style>
---
+++ src/lib/DomainesContent.svelte
@@ -0,0 +1,190 @@
+<script lang="ts">
+  import type { ImageType } from "@/middleware/imageType";
+
+  interface ColumnsType {
+    image: ImageType;
+    image_webp: ImageType;
+    texte: string;
+  }
+
+  import { imageFormatter } from "@/stores/imageFormatter";
+  import { webpSupported, checkWebpSupport } from "@/stores/webpSupported";
+  export let titre: string;
+  export let columns: ColumnsType[];
+  export let texte: string;
+  export let imageTitre: string;
+  export let bulletColor: string;
+  export let borderColor: string;
+
+  checkWebpSupport();
+</script>
+
+<div
+  class="container"
+  style=" --theme-border: {borderColor}; --theme-bullet: {bulletColor}"
+>
+  <h2 style="background: url(/{imageTitre}) no-repeat">{titre}</h2>
+  <div class="column">
+    {#each columns as column}
+      <div class="row">
+        <div class="image-row-container">
+          <div class="background" />
+          {#if column.image_webp}
+            <img
+              src={imageFormatter(
+                { webP: column.image_webp.url, old: column.image.url },
+                webpSupported
+              )}
+              alt={column.image.url}
+              title={column.image.title}
+            />
+          {:else}
+            <img
+              src={column.image.url}
+              alt={column.image.alt}
+              title={column.image.title}
+            />
+          {/if}
+        </div>
+        <div class="texte-row-container">
+          {@html column.texte}
+        </div>
+      </div>
+    {/each}
+  </div>
+  <div class="bordered-texte">{@html texte}</div>
+</div>
+
+<style lang="scss">
+  h2 {
+    font-size: 26px;
+    font-weight: 500;
+    position: relative;
+    display: grid;
+    grid-template-columns: 55.87px 1fr;
+    gap: 1rem;
+    align-items: center;
+    text-transform: uppercase;
+
+    @media (min-width: 768px) {
+      width: 80%;
+      margin-inline: auto;
+    }
+
+    @media (min-width: 1330px) {
+      grid-template-columns: 77.55px 1fr;
+      gap: 2.92rem;
+    }
+
+    &::before {
+      content: "";
+      background-size: contain;
+      width: 100%;
+      height: 48.49px;
+      margin-right: 1rem;
+
+      @media (min-width: 1330px) {
+        width: 100%;
+        height: 64.99px;
+        margin-right: 2.92rem;
+      }
+    }
+  }
+
+  .column {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 2.8rem;
+    margin-block: 4.7rem 3.2rem;
+
+    @media (min-width: 768px) {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+
+  .row {
+    display: flex;
+    flex-direction: column;
+    gap: 2.6rem;
+
+    :global(p) {
+      font-size: 2rem;
+      font-weight: 500;
+
+      @media (min-width: 1330px) {
+        font-size: 2.4rem;
+      }
+    }
+
+    :global(ul) {
+      margin-top: 2.3rem;
+      font-weight: 500;
+      list-style: none;
+      display: flex;
+      flex-direction: column;
+      gap: 1em;
+
+      @media (min-width: 1330px) {
+        font-size: 2rem;
+      }
+    }
+
+    :global(li) {
+      display: grid;
+      grid-template-columns: 15px 1fr;
+      gap: 2.1rem;
+      align-items: start;
+      line-height: 1.3;
+
+      &::before {
+        content: "";
+        background: var(--theme-bullet);
+        margin-top: 0.3em;
+        width: 15px;
+        height: 15px;
+        border-radius: 50%;
+        display: flow-root;
+      }
+    }
+  }
+
+  .image-row-container {
+    position: relative;
+    max-width: 97%;
+    margin-inline: auto;
+
+    @media (min-width: 768px) {
+      max-width: 100%;
+      width: 100%;
+    }
+
+    img {
+      position: relative;
+      z-index: 1;
+      width: 100%;
+    }
+  }
+
+  .background {
+    background: white;
+    border: 1px solid var(--theme-border);
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    right: -0.4em;
+    bottom: -0.4em;
+  }
+
+  .bordered-texte {
+    border: 1px solid var(--theme-border);
+    padding: 3rem;
+    display: flex;
+    flex-direction: column;
+    gap: 3rem;
+    font-weight: 500;
+
+    @media (min-width: 1330px) {
+      font-size: 2rem;
+    }
+  }
+</style>
